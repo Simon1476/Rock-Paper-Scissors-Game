f4f378af83d9e42588e7d812eac7c7b4aabd4b31
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-08-24 20:47:52 +0900

    Add playRound function
    It takes two parameters, player Selection and computer Selection, and returns a string indicating who won.

--- main.js
+++ main.js
@@ -4,4 +4,27 @@ function getComputerChoice() {
   return randomChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toLowerCase();
+  const computer = computerSelection;
+
+  let gameResult = "";
+
+  if (player === computer) return `You Draw!`;
+  else if (player === "rock" && computer === "scissors") gameResult = "player";
+  else if (player === "paper" && computer === "rock") gameResult = "player";
+  else if (player === "scissors" && computer === "paper") gameResult = "player";
+  else gameResult = "computer";
+
+  return gameResult === "player"
+    ? `You Win! ${player} beats ${computer}`
+    : `Computer Win! ${computer} beats ${player}`;
+}
+
+const playerSelection = prompt(
+  "Which of the rock paper scissors will you serve?"
+);
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
