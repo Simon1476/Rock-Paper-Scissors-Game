a46d7d983fc7e080019b615f555dceedb113248a
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-08-27 11:26:55 +0900

    Add imageChange function and Startgame function
    When the user clicks the button, the image changes
    Run the game through the Users and Computers section

--- main.js
+++ main.js
@@ -1,30 +1,88 @@
+let computerSelection = "";
+let playerSelection = "";
+
+let totalRound = 5;
+let userCount = 0;
+let computerCount = 0;
+
+const rockBtn = document.querySelector(".rock-btn");
+const paperBtn = document.querySelector(".paperBtn-btn");
+const scissorsBtn = document.querySelector(".scissors-btn");
+
+const playerQuestionImg = document.querySelector(".user .question-btn img");
+const computerQuestionImg = document.querySelector(
+  ".computer .question-btn img"
+);
+
+const allBtn = document.querySelectorAll(".btn-box button");
+const playerAllBtn = document.querySelectorAll(".user .btn-box button");
+
+playerAllBtn.forEach((button) => {
+  button.addEventListener("click", startGame);
+});
+
+function imageChange(playerSelection, computerSelection) {
+  switch (playerSelection) {
+    case "rock":
+      playerQuestionImg.src = "./image/rocks.png";
+      break;
+    case "paper":
+      playerQuestionImg.src = "./image/paper.png";
+      break;
+    case "scissors":
+      playerQuestionImg.src = "./image/scissor.png";
+      break;
+  }
+
+  switch (computerSelection) {
+    case "rock":
+      computerQuestionImg.src = "./image/rocks.png";
+      break;
+    case "paper":
+      computerQuestionImg.src = "./image/paper.png";
+      break;
+    case "scissors":
+      computerQuestionImg.src = "./image/scissor.png";
+      break;
+  }
+}
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   let randomChoice = choices[Math.floor(Math.random() * choices.length)];
   return randomChoice;
 }
 
+function startGame(e) {
+  playerSelection = e.target.alt;
+  computerSelection = getComputerChoice();
+  if (playerSelection && computerSelection) game();
+}
+
 function playRound(playerSelection, computerSelection) {
-  const player = playerSelection.toLowerCase();
+  const player = playerSelection;
   const computer = computerSelection;
 
   let gameResult = "";
 
-  if (player === computer) return `You Draw!`;
+  if (player === computer) gameResult = "draw";
   else if (player === "rock" && computer === "scissors") gameResult = "player";
   else if (player === "paper" && computer === "rock") gameResult = "player";
   else if (player === "scissors" && computer === "paper") gameResult = "player";
   else gameResult = "computer";
 
-  return gameResult === "player"
-    ? `You Win! ${player} beats ${computer}`
-    : `Computer Win! ${computer} beats ${player}`;
+  // return gameResult === "player"
+  //   ? `You Win! ${player} beats ${computer}`
+  //   : `Computer Win! ${computer} beats ${player}`;
+  return gameResult;
 }
 
-const playerSelection = prompt(
-  "Which of the rock paper scissors will you serve?"
-);
+function game() {
+  imageChange(playerSelection, computerSelection);
+  --totalRound;
+  let gameResult = playRound(playerSelection, computerSelection);
+  if (gameResult === "draw") return;
+  gameResult === "player" ? ++userCount : ++computerCount;
+}
 
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+console.log(`${totalRound} ${userCount} ${computerCount}`);
