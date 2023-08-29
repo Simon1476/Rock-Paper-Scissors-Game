972e9dd1bb9248c9a213c4de971161b33ad40707
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-08-29 14:29:11 +0900

    Fix Sound Playback Issue with Resetting currentTime
    If current time = 0 is not set, the sound is not played after clicking the button once

--- sound.js
+++ sound.js
@@ -2,6 +2,11 @@ const winSound = new Audio("./sound/win.mp3");
 const loseSound = new Audio("./sound/lose.mp3");
 const clickSound = new Audio("./sound/click.mp3");
 
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
 export function playWin() {
   playSound(winSound);
 }
